2a08944aaedef6e0c34cfcdff5c163b46d024f1a
James Stallworthy <stallwo2@uni.coventry.ac.uk>
2015-11-23 13:20:50 +0000

    Commit

--- login.js
+++ login.js
@@ -14,16 +14,22 @@ function load(){
     console.log("Running load function");
     gapi.client.load('games','v1',function(response) {
         // Ready to make calls!
-        if(response == null)
-            console.log("Everything is good so far....");
-         var request = gapi.client.games.leaderboards.list(
+        if(response == null){
+            loadLeaderboard();
+        }
+      });
+  
+   
+}
+
+function loadLeaderboard(){
+
+ var request = gapi.client.games.leaderboards.list(
       {maxResults: 5}
     );
     request.execute(function(response) {
         console.log('Leaderboard data', response);
       // Do something interesting with the response
     });
-      });
-  
-   
+    
 }
