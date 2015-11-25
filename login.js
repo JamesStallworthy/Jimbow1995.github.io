21add89883de553187b54f8cd96e54f58e3fa951
James Stallworthy <stallwo2@uni.coventry.ac.uk>
2015-11-25 14:00:05 +0000

    Commit

--- login.js
+++ login.js
@@ -34,8 +34,6 @@ function loadLeaderboard(){
 //    });
     var json={};
     json.leaderboardId='CgkIw5Xv3M4GEAIQAQ';
-    json.collection='PUBLIC';
-    json.timeSpan='ALL_TIME';
     var request = gapi.client.games.scores.listWindow(json);
     request.execute(function(response) {
         console.log('Leaderboard data', response);
