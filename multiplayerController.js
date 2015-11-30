c7e4dc97a9ac259160bb7dbd9a1d69ea1c89b87b
James Stallworthy <stallwo2@uni.coventry.ac.uk>
2015-11-30 21:50:06 +0000

    Commit

--- multiplayerController.js
+++ multiplayerController.js
@@ -14,6 +14,7 @@ function multiplayerController(){
         createGame();
     }
     else if (gamestate == "takeTurn"){
+        Grid.drawGrid();
         console.log("Take your turn");
     }
     else if (gamestate == "waiting"){
