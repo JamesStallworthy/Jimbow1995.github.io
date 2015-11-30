d60f76c7b59ad3fb278ce589c81938a3aa3a775e
James Stallworthy <stallwo2@uni.coventry.ac.uk>
2015-11-30 18:02:00 +0000

    Commit

--- leaderboardController.js
+++ leaderboardController.js
@@ -31,7 +31,7 @@ leaderboardController.prototype.loadLeaderboard = function(){
     request.execute(function(response) {
         this.numOfScores = response.items.length;
         for (var i = 0; i < response.items.length; i++){
-            this.score[0] = response.items[0].formattedScore;
+            this.score[0] = i;
         }
         console.log('score', this.score);
         
