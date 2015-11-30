3b192817788699b24cb44c09977ece1b8568502e
James Stallworthy <stallwo2@uni.coventry.ac.uk>
2015-11-30 18:24:26 +0000

    Commit

--- leaderboardController.js
+++ leaderboardController.js
@@ -29,8 +29,8 @@ leaderboardController.prototype.loadLeaderboard = function(){
     json.timeSpan='ALL_TIME';
     var request = gapi.client.games.scores.listWindow(json);
     request.execute(function(response) {
-        
+            console.log("1");
         }
     );
-    console.log(score);
+    console.log("2");
 }
