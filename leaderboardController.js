92292978bd56540fad335d2fb8c59bfa52f3d941
James Stallworthy <stallwo2@uni.coventry.ac.uk>
2015-11-30 19:29:36 +0000

    Commit

--- leaderboardController.js
+++ leaderboardController.js
@@ -1,37 +1,18 @@
-//function leaderboardController(){
-//}
-//
-//leaderboardController.prototype.submitScore = function(id,score){
-//     var request = gapi.client.games.scores.submit(
-//        {leaderboardId: id,
-//         timeSpan : "ALL_TIME",
-//        score: score}
-//    );
-//    request.execute(function(response) {
-//        if(response == null){
-//            console.log("New score submitted");
-//        }
-//        else{
-//            console.log("error", response);
-//        } 
-//    });
-//}
-//
-//leaderboardController.prototype.loadLeaderboard = function(id){
-//    leaderBoardLoading = true;
-//    console.log("Loading leaderboard");
-//    var json={};
-//    json.leaderboardId=id;
-//    json.collection='PUBLIC';
-//    json.timeSpan='ALL_TIME';
-//    var request = gapi.client.games.scores.listWindow(json);
-//    request.execute(function(response) {
-//            leaderBoardDataLoaded = true;
-//            console.log("Data loaded");
-//            leaderboardData = response.items;
-//        }
-//    );
-//}
+function submitScore(id, score){
+     var request = gapi.client.games.scores.submit(
+        {leaderboardId: id,
+         timeSpan : "ALL_TIME",
+        score: score}
+    );
+    request.execute(function(response) {
+        if(response == null){
+            console.log("New score submitted");
+        }
+        else{
+            console.log("error", response);
+        } 
+    });
+}
 
 function loadLeaderboard(id){
     console.log("loading leaderboard: ", id);
@@ -47,6 +28,7 @@ function loadLeaderboard(id){
             leaderboardData = response.items;
             leaderboardDataLoaded = true;
             leaderboardDataLoading = false;
+            submitScore('CgkIw5Xv3M4GEAIQAQ', 14);
         }
     );
 }
