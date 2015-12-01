0e5913c498a964c244c6c2b5662982dbb3249d1d
James Stallworthy <stallwo2@uni.coventry.ac.uk>
2015-12-01 19:25:29 +0000

    Commit

--- multiplayerCom.js
+++ multiplayerCom.js
@@ -1,13 +1,16 @@
+
+/** Creates a game and invite the ID stored in inviteID */
 function createGame(){
     player = "p_1";
     participant = "p_2";
     gamestate = "processing";
+    
     var request = gapi.client.games.turnBasedMatches.create({
         "kind" : "games#turnBasedMatchCreateRequest",
-        "variant": 0,
         "invitedPlayerIds" : [inviteID],
         "requestID" : Math.floor(Math.random * 1000000000000)
     });
+    
     request.execute(function(response)
     {
         console.log("Game created");
@@ -19,44 +22,20 @@ function createGame(){
     }); 
 }
 
-function initGame(){
-    var request = gapi.client.games.turnBasedMatches.takeTurn(
-       {"matchId" : matchID},
-       {
-        "kind": "games#turnBasedMatchTurn",
-        "data":
-        {
-            "kind": "games#turnBasedMatchDataRequest",
-            "data": btoa("initData")
-        },
-        "pendingParticipantId": "p_1",
-        "matchVersion": 1,
-    });
-    request.execute(function(response){
-        console.log("game inited");
-        gamestate = "takeTurn";
-        console.log(response);
-    });
-}
 
-function activeGames(){
+function joinGame(){
+    player = "p_2";
+    participant = "p_1";
     var request = gapi.client.games.turnBasedMatches.list();
     request.execute(function(response){
-        console.log(response);
-    });
-}
-
-function cancelGame(){
-    var request = gapi.client.games.turnBasedMatches.list();
-    request.execute(function(response){
-        var newRequest = gapi.client.games.turnBasedMatches.cancel(
+        matchID = response.items[0].matchId;
+        var newRequest = gapi.client.games.turnBasedMatches.join(
             {
                 "matchId" : response.items[0].matchId
             });
         
         newRequest.execute(function(response){
-            console.log(response);
-            console.log("Game cancelled");
+            console.log("Game joined");
         });
     });
 }
@@ -82,22 +61,6 @@ function takeTurn(dataToSend){
     });
 }
 
-function joinGame(){
-    player = "p_2";
-    participant = "p_1";
-    var request = gapi.client.games.turnBasedMatches.list();
-    request.execute(function(response){
-        matchID = response.items[0].matchId;
-        var newRequest = gapi.client.games.turnBasedMatches.join(
-            {
-                "matchId" : response.items[0].matchId
-            });
-        
-        newRequest.execute(function(response){
-            console.log("Game joined");
-        });
-    });
-}
 
 function getData(){
     gamestate == "processing";
@@ -117,3 +80,71 @@ function getData(){
         }
     });
 }
+
+
+function activeGames(){
+    var request = gapi.client.games.turnBasedMatches.list();
+    request.execute(function(response){
+        console.log(response);
+    });
+}
+
+function cancelGame(){
+    var request = gapi.client.games.turnBasedMatches.list();
+    request.execute(function(response){
+        var newRequest = gapi.client.games.turnBasedMatches.cancel(
+            {
+                "matchId" : response.items[0].matchId
+            });
+        
+        newRequest.execute(function(response){
+            console.log(response);
+            console.log("Game cancelled");
+        });
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function initGame(){
+//    var request = gapi.client.games.turnBasedMatches.takeTurn(
+//       {"matchId" : matchID},
+//       {
+//        "kind": "games#turnBasedMatchTurn",
+//        "data":
+//        {
+//            "kind": "games#turnBasedMatchDataRequest",
+//            "data": btoa("initData")
+//        },
+//        "pendingParticipantId": "p_1",
+//        "matchVersion": 1,
+//    });
+//    request.execute(function(response){
+//        console.log("game inited");
+//        gamestate = "takeTurn";
+//        console.log(response);
+//    });
+//}
