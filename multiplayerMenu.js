2b67c666920346917057c0ab9ae86250b398912a
James Stallworthy <stallwo2@uni.coventry.ac.uk>
2015-12-02 15:10:44 +0000

    Commit

--- multiplayerMenu.js
+++ multiplayerMenu.js
@@ -43,6 +43,7 @@ multiplayerMenu.prototype.clickedInvite = function(x,y) {
     var pos = Math.round((y-10) / 30);
     this.selected = pos;
     if (x > 320*ratioWidht){
+        console.log(usersFriends);
         inviteID = usersFriends[this.selected].Id;
        createGame();
     }
