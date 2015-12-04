df2031a26e30afd4547c8f9289a0853a790f51cf
James Stallworthy <stallwo2@uni.coventry.ac.uk>
2015-12-04 10:32:04 +0000

    Commit

--- multiplayerMenu.js
+++ multiplayerMenu.js
@@ -19,10 +19,6 @@ multiplayerMenu.prototype.loadImg = function() {
 
 multiplayerMenu.prototype.drawCreateJoin = function () {
     ctx.drawImage(this.createJoin, 0, 0);
-    ctx.font = 'bold 10pt Calibri';
-    for(var i=0; i<invitedToList.length; i++){
-        ctx.fillText(invitedToList[i].userMatchStatus, 60, 30+(i*20));
-    }
 }
 
 multiplayerMenu.prototype.drawInviteList = function(){
@@ -41,17 +37,9 @@ multiplayerMenu.prototype.drawInviteList = function(){
 
 multiplayerMenu.prototype.drawGameList = function(){
     ctx.drawImage(this.GameList, 0, 0);
-    if (x < 320*ratioWidht){
-        var pos = Math.round((y-(30*ratioHeight)) / (20*ratioHeight));
-        this.selected = pos; 
-    }
-    if (x > 320*ratioWidht){
-        if (x < 240*ratioHeight){
-                
-        }
-        else{
-            
-        }
+    ctx.font = 'bold 10pt Calibri';
+    for(var i=0; i<invitedToList.length; i++){
+        ctx.fillText(invitedToList[i].userMatchStatusy, 60, 30+(i*20));
     }
 }
 
