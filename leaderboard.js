5d8821f09ebaf9085b7f525823dce4087c82db4b
langynom <langdon3@uni.coventry.ac.uk>
2015-12-07 18:32:12 +0000

    Commit

--- leaderboard.js
+++ leaderboard.js
@@ -13,12 +13,15 @@ leaderBoard.prototype.loadImg = function() {
 }
 
 leaderBoard.prototype.drawImgBoard = function () {
-    //ctx.drawImage(this.boardImg, 0, 0);
-    ctx.font = 'bold 10pt Calibri';
+    ctx.drawImage(this.boardImg, 0, 0);
+    ctx.font = 'bold 20pt Calibri';
     for(var i=0; i<leaderboardData.length; i++)
     {
+        ctx.textAlign="center"; 
         ctx.fillText(leaderboardData[i].formattedScoreRank, 60, 80+(i*20));
+        ctx.textAlign="left"; 
         ctx.fillText(leaderboardData[i].formattedScore, 80, 80+(i*20));
+        ctx.textAlign="center"; 
         ctx.fillText(leaderboardData[i].player.name.givenName, 100, 80+(i*20));
     }
 }
