e54e00d3f763558d3f5f88ab545ef563fd48cec7
langynom <langdon3@uni.coventry.ac.uk>
2015-12-09 10:02:53 +0000

    Commit

--- login.js
+++ login.js
@@ -27,14 +27,8 @@ function load(){
        var request = gapi.client.plus.people.list({"collection": "visible","userId": "me"});
        request.execute(function(response){
            console.log(response);
-           usersFriends=response.items
-           var newRequest = gapi.client.plus.people.userId({userId: "me"});
-           newRequest.execute(function(response){
-           console.log(response);
-    });
-       });
+           usersFriends=response.items});
       });
-    
-    
+    }
       gamestate = "menu";
 }
